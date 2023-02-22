804f46dfe819b0b1ad389194d11cfdc262584fa4
Thiago Loiola <bens.quin@gmail.com>
2023-02-22 19:16:37 -0300

    criando e removendo mosquito a cada clico de tempo

--- jogo.js
+++ jogo.js
@@ -12,6 +12,11 @@ ajustaTamanhoPalcoJogo()
 
 
 function posicaoRandomica() {
+
+    if(document.getElementById('mosquito')) {
+    document.getElementById('mosquito').remove()
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -24,6 +29,7 @@ function posicaoRandomica() {
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px' 
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
 
 
     document.body.appendChild(mosquito)
